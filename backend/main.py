fbbd767448b9595f9daa86fec4231e9a2fcd81c3
Sebastian Christen <sch140456@stud.gibb.ch>
2024-12-26 17:17:49 +0100

    implement new frontend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
-# main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from endpoints.messages import router as messages_router
 
 app = FastAPI()
 
-app.include_router(messages_router, prefix="/messages", tags=["messages"] ) # endpoints/messages.py
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # Nur Anfragen von localhost:8080 erlauben
+)
+
+app.include_router(messages_router, prefix="/messages", tags=["messages"])
