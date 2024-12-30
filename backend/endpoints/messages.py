dc9cb37f33540f19191ab6fde4fa826dc23afe67
Sebastian Christen <sch140456@stud.gibb.ch>
2024-12-30 01:15:23 +0100

    feat(backend): implement POST requests

--- backend/endpoints/messages.py
+++ backend/endpoints/messages.py
@@ -15,17 +15,8 @@ async def get_messages(db: Database = Depends(get_db)):
     return [Message(**msg) for msg in messages]
 
 
-# # GET ONE
-# @router.get("/{message_id}", response_model=Message)
-# async def get_message(message_id: str, db: Database = Depends(get_db)):
-#     message = db.messages.find_one({"id": message_id})
-#     if not message:
-#         raise HTTPException(status_code=404, detail="Message not found")
-#     return Message(**message)
-
-
-# # POST
-# @router.post("/", response_model=Message)
-# async def create_message(message: Message, db: Database = Depends(get_db)):
-#     db.messages.insert_one(message.dict())
-#     return message
+# POST ONE
+@router.post("/", response_model=Message)
+async def create_message(message: Message, db: Database = Depends(get_db)):
+    db.messages.insert_one(message.dict())
+    return message
