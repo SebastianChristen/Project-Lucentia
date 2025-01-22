bbac810d9df750bd4b0960edb1586a870d93b576
Christen Sebastian <Sebastian.Christen@swisscom.com>
2025-01-22 16:13:07 +0100

    implement chat model

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 import uuid
 import time
 from pydantic import BaseModel, Field
@@ -8,6 +8,11 @@ class Message(BaseModel):
     sender: str
     message: str
     sent_at: float = Field(default_factory=lambda: time.time())
+
+class Chat(BaseModel):
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
+    members: List[str]
+    messages: List[Message] = Field(default_factory=list)
 
 class User(BaseModel):
     id: uuid.UUID = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
